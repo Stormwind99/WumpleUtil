58b8513ac41cc027718178d00a70233e37b705bc
Jason Spangler <jasons@wumple.com>
2018-07-20 19:13:27 -0500

    Jar signing and Curseforge upload deps

--- src/main/java/com/wumple/util/WumpleUtil.java
+++ src/main/java/com/wumple/util/WumpleUtil.java
@@ -1,14 +1,16 @@
 package com.wumple.util;
 
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, dependencies = Reference.DEPENDENCIES, updateJSON=Reference.UPDATEJSON)
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, dependencies = Reference.DEPENDENCIES, updateJSON = Reference.UPDATEJSON, certificateFingerprint=Reference.FINGERPRINT)
 public class WumpleUtil
 {
     @Mod.Instance(Reference.MOD_ID)
@@ -29,4 +31,18 @@ public class WumpleUtil
     @EventHandler
     public void postInit(FMLPostInitializationEvent event)
     { }
+
+    @EventHandler
+    public void onFingerprintViolation(FMLFingerprintViolationEvent event)
+    {
+        if (logger == null)
+        {
+            logger = LogManager.getLogger(Reference.MOD_ID);
+        }
+        if (logger != null)
+        {
+            logger.warn("Invalid fingerprint detected! The file " + event.getSource().getName() + " may have been tampered with. This version will NOT be supported by the author!");
+            logger.warn("Expected " + event.getExpectedFingerprint() + " found " + event.getFingerprints().toString());
+        }
+    }
 }
