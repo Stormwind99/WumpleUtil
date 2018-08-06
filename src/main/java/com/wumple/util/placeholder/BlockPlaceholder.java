8c5249b5dc2539093d8b58acf2e3477fd8b18eea
Jason Spangler <jasons@wumple.com>
2018-08-06 16:16:22 -0500

    Placeholder registration

--- src/main/java/com/wumple/util/placeholder/BlockPlaceholder.java
+++ src/main/java/com/wumple/util/placeholder/BlockPlaceholder.java
@@ -1,11 +1,22 @@
 package com.wumple.util.placeholder;
 
+import com.wumple.util.misc.RegistrationHelpers;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
+import net.minecraftforge.registries.IForgeRegistry;
 
 public class BlockPlaceholder extends Block implements IBlockPlaceholder
 {
+    public static void register(final IForgeRegistry<Block> registry)
+    {
+        RegistrationHelpers.regHelper(registry, new BlockPlaceholder());
+    }
+    
+    public BlockPlaceholder()
+    { super(Material.AIR); }
+    
     public BlockPlaceholder(Material blockMaterialIn, MapColor blockMapColorIn)
     {
         super(blockMaterialIn, blockMapColorIn);
