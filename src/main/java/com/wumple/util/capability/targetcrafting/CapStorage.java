9c6a8036560d6b81300593c1f190c35679d08f2a
Jason Spangler <jasons@wumple.com>
2019-12-25 10:43:49 -0600

    Another attempt to avoid null NBT crash from https://github.com/Stormwind99/FoodFunk/issues/56

--- src/main/java/com/wumple/util/capability/targetcrafting/CapStorage.java
+++ src/main/java/com/wumple/util/capability/targetcrafting/CapStorage.java
@@ -1,6 +1,7 @@
 package com.wumple.util.capability.targetcrafting;
 
 import net.minecraft.nbt.NBTBase;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.Capability.IStorage;
@@ -15,7 +16,13 @@ public class CapStorage<U extends IContainerCraftingOwner> implements IStorage<U
             return instance.serializeNBT();
         }
 
-        return null;
+        // Attempted workaround for https://github.com/Stormwind99/FoodFunk/issues/56
+        // Some mods are trying to serialize capabilities during Forge mod init!
+        // Then they crash with foodfunk:rot NBT data
+        // Used to:
+        // return null;
+        NBTTagCompound tags = new NBTTagCompound();
+        return tags;
     }
 
     @Override
